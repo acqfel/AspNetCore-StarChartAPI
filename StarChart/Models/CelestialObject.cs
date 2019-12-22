f6fcdf8c66d7d47f221c6a5f49ba2c2ccad913a9
Felipe Acquati <felipe_acq@yahoo.com.br>
2019-12-22 19:15:35 -0300

    Fixed: Id property

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,7 +9,7 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int Id;
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
